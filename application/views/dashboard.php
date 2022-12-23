1dbafe294b72a6e71cf75b24b7db8885046ff18b
failamir <fail.amir@students.amikom.ac.id>
2022-12-23 20:27:30 +0700

    Update dashboard.php

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -43,7 +43,7 @@
             options: {
                 title: {
                     display: true,
-                    text: 'Weather'
+                    text: ''
                 }
             }
         });
